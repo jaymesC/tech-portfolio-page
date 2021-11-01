8d791313c8142b866d5f30a0ff839de31250c601
jaymesC <clementjaymes@gmail.com>
2021-11-01 16:47:59 -0700

    updated project files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import '../styles/index.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp() {
+  return (
+    <div className="container">
+      <h1>
+        Exp<span className="underline">erie</span>nce
+      </h1>
+      <div className="content">
+        <div className="names">
+          <h2>TOMMY</h2>
+        </div>
+        <div className="position">
+          <h4>Full stack web developer</h4>
+          <p>helo</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,11 @@
 
 import styles from '../styles/Home.module.css'
+import MyApp from './_app';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>
-        Exp<span className={styles.underline}>erie</span>nce
-      </h1>
-      <div className={styles.content}>
-        <div className={styles.names}>
-          <h2>TOMMY</h2>
-        </div>
-        <div className={styles.position}>
-          <h4>Full stack web developer</h4>
-          <p>helo</p>
-        </div>
-      </div>
-    </div>
+  <div>
+    < MyApp />
+  </div>
   );
 }
