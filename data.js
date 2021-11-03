a82488c9a263af478e0d0f7dbfa8fa1d64f2aeec
jaymesC <clementjaymes@gmail.com>
2021-11-03 05:52:13 -0700

    updated project files

--- data.js
+++ data.js
@@ -1,25 +1,25 @@
 export const data = [
-//   {
-//     position: "Full Stack Web Developer",
-//     name: "TOMMY",
-//     duration: "December 2015 - Present",
-//     descriptions: [
-//       "Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge disrupt.",
-//       "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
-//       "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
-//     ],
-//   },
-//   {
-//     position: "Front-End Engineer",
-//     name: "BIGDROP",
-//     duration: "May 2015 - December 2015",
-//     descriptions: [
-//       "Hashtag drinking vinegar scenester mumblecore snackwave four dollar toast, lumbersexual XOXO. Cardigan church-key pabst, biodiesel vexillologist viral squid.",
-//       "Franzen af pitchfork, mumblecore try-hard kogi XOXO roof party la croix cardigan neutra retro tattooed copper mug. Meditation lomo biodiesel scenester",
-//       "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
-//       "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
-//     ],
-//   },
+  {
+    position: "Full Stack Web Developer",
+    name: "TOMMY",
+    duration: "December 2015 - Present",
+    descriptions: [
+      "Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge disrupt.",
+      "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
+      "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
+    ],
+  },
+  {
+    position: "Front-End Engineer",
+    name: "BIGDROP",
+    duration: "May 2015 - December 2015",
+    descriptions: [
+      "Hashtag drinking vinegar scenester mumblecore snackwave four dollar toast, lumbersexual XOXO. Cardigan church-key pabst, biodiesel vexillologist viral squid.",
+      "Franzen af pitchfork, mumblecore try-hard kogi XOXO roof party la croix cardigan neutra retro tattooed copper mug. Meditation lomo biodiesel scenester",
+      "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
+      "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
+    ],
+  },
   {
     position: "Engineering Intern",
     name: "CUKER",
@@ -32,3 +32,5 @@ export const data = [
     ],
   },
 ];
+
+console.log(data[0].name)
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,15 @@ import "../styles/globals.css";
 import "../styles/index.css";
 import { ChevronDoubleRightIcon } from "@heroicons/react/solid";
 import { data } from "../data";
+import React from "react";
 
 function MyApp() {
+  const [count, setCount] = React.useState(0);
+
+  const handleClick = function () {
+    setCount(count + 1);
+  }
+
   return (
     <div className="container">
       <h1>
@@ -11,26 +18,20 @@ function MyApp() {
       </h1>
       <div className="content">
         <div className="names">
-          <h2>TOMMY</h2>
-          <h2>BIGDROP</h2>
+          <h2 onClick={handleClick}>TOMMY</h2>
+          <h2 oncClick={() => setCount(count -1)}>BIGDROP</h2>
           <h2>CUKER</h2>
         </div>
-        {data.map((person) => (
-        <div className="position" key={person.name}>
-          <h4>{person.position}</h4>
-          <span>{person.name}</span>
-          <p>{person.duration}</p>
-          {person.descriptions.map((x, index) => (
-            <div className="write-up" key={index}>
-              <ChevronDoubleRightIcon className="icon" />
-              <p>
-               {x}
-              </p>
-            </div>
-          ))}
-        </div>
-
-        ))}
+          <div className="position" >
+            <h4>{data[count].position}</h4>
+            <span>{data[count].name}</span>
+            <p>{data[count].duration}</p>
+            
+              <div className="write-up" >
+                <ChevronDoubleRightIcon className="icon" />
+                <p>{data.descriptions}</p>
+              </div>
+          </div>
       </div>
       <div className="button-container">
         <button>MORE INFO</button>
